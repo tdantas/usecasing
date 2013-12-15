b95f0d623cee6fc4e940e6a9356c3f8a296a3658
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-12-15 23:10:26 +0000

    refactoring module to use ClassMethod module

--- lib/usecasing/base.rb
+++ lib/usecasing/base.rb
@@ -2,46 +2,51 @@ module UseCase
 
   module BaseClassMethod
 
-    def depends(*deps)
-      @dependencies ||= []
-      @dependencies.push(*deps)
+    def self.included(base)
+      base.extend ClassMethods
     end
 
-    def dependencies
-      return [] unless superclass.ancestors.include? UseCase::Base
-      value = (@dependencies && @dependencies.dup || []).concat(superclass.dependencies)
-      value
-    end
-
-    def perform(context = {})
-      context = Context.new(context)
-      execution_order = build_execution_order(self, {})
-      execution_order.each do |usecase|
-        break unless context.success?
-        usecase.new(context).perform 
+    module ClassMethods
+      def depends(*deps)
+        @dependencies ||= []
+        @dependencies.push(*deps)
       end
 
-      context
-    end
-
-    private
-
-    def build_execution_order(start_point, visited)
-      raise StandardError.new("cyclic detected: #{start_point} in #{self}") if visited[start_point]
-      visited[start_point] = true
-      return [start_point] if start_point.dependencies.empty?
-
-      start_point.dependencies.each do |point|
-        build_execution_order(point, visited).unshift point
+      def dependencies
+        return [] unless superclass.ancestors.include? UseCase::Base
+        value = (@dependencies && @dependencies.dup || []).concat(superclass.dependencies)
+        value
       end
 
+      def perform(context = {})
+        context = Context.new(context)
+        execution_order = build_execution_order(self, {})
+        execution_order.each do |usecase|
+          break unless context.success?
+          usecase.new(context).perform 
+        end
+
+        context
+      end
+
+      private
+
+      def build_execution_order(start_point, visited)
+        raise StandardError.new("cyclic detected: #{start_point} in #{self}") if visited[start_point]
+        visited[start_point] = true
+        return [start_point] if start_point.dependencies.empty?
+
+        start_point.dependencies.each do |point|
+          build_execution_order(point, visited).unshift point
+        end
+      end
     end
-    
+
   end
 
-
   class Base
-    extend BaseClassMethod
+
+    include BaseClassMethod
 
     attr_reader :context
     def initialize(context)
