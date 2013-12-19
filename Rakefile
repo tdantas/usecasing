06e6c5b34774ea22f9b6ba0500ea319847466441
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-12-19 00:08:18 +0000

    add travis ci

--- Rakefile
+++ Rakefile
@@ -1,4 +1,6 @@
 require "bundler/gem_tasks"
 
 require 'rspec/core/rake_task'
+
+task :default => :test
 RSpec::Core::RakeTask.new(:test)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'usecasing'
-require 'pry'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
